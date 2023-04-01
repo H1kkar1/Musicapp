6eef10351c1e713624eed4b4e16acaa6e00db70d
H1kkar1 <128414242+H1kkar1@users.noreply.github.com>
2023-04-01 21:55:00 +0300

    Добавлено, имя песни, расширение

--- Musicapp/MainWindow.xaml.cs
+++ Musicapp/MainWindow.xaml.cs
@@ -22,9 +22,10 @@ namespace Musicapp
 { 
     public partial class MainWindow : Window
     {
-        string PATH_to_mf = @"";
+        Dictionary<string,string> PATH = new Dictionary<string, string>();
         AudioFileReader audioFile;
         WaveOutEvent outputDevice;
+        //TimeSpan duration = outputDevice.TotalTime;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,20 +33,32 @@ namespace Musicapp
 
         public string GetPath()
         {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Filter = "MP3|*.mp3|WAV|*.wav"; // Filter files by extension
-            Nullable<bool> result = dlg.ShowDialog();
-            if (result == true)
+            try
             {
-                return dlg.FileName;
-            }
-            return null;
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "MP3|*.mp3|WAV|*.wav"; // Filter files by extension
+                Nullable<bool> result = dlg.ShowDialog();
+                if (result == true)
+                {
+                    return dlg.FileName;
+                }
+                return null;
+            } catch (Exception f) { return null; }
         }
         private void Button_Change(object sender, RoutedEventArgs e)
         {
-            PATH_to_mf = @"";
-            PATH_to_mf += GetPath();
-            pole.Text = PATH_to_mf;
+            PATH.Clear();
+            string a = GetPath();
+            if (a != null)
+            {
+                string[] name = a.Split(new char[] { (char)92 });
+                string[] expansion = name[name.Length - 1].Split(new char[] { '.' });
+                PATH.Add("path",a);
+                PATH.Add("name", name[name.Length - 1].Substring(0, name[name.Length - 1].Length - 4));
+                PATH.Add("expansion", expansion[1]);
+                pole.Text = PATH["path"] + "\n\n" + PATH["name"] + "\n\n" + PATH["expansion"];
+                treck_name.Content = PATH["name"];
+            }
         }
         private void Button_Play(object sender, RoutedEventArgs e)
         {
@@ -56,15 +69,17 @@ namespace Musicapp
             }
             if (audioFile == null)
             {
-                audioFile = new AudioFileReader(PATH_to_mf);
+                audioFile = new AudioFileReader(@PATH["path"]);
                 outputDevice.Init(audioFile);
             }
             outputDevice.Play();
+           
         }
 
         private void Button_Stop(object sender, RoutedEventArgs e)
         {
-            outputDevice.Stop();
+            outputDevice.Pause();
+           
         }
         private void OnPlaybackStopped(object sender, StoppedEventArgs args)
         {
@@ -72,6 +87,11 @@ namespace Musicapp
             outputDevice = null;
             audioFile.Dispose();
             audioFile = null;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
